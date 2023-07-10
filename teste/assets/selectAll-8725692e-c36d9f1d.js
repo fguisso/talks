acd9bf65870c5a9f40857265b5c0c20c6e4b0294
fguisso <fguisso@gmail.com>
2023-07-10 02:26:10 +0000

    Deploying to gh-pages from @ fguisso/talks@aab071c4341bfa4c96e5579f4939ad7999cf3979 🚀

---
+++ teste/assets/selectAll-8725692e-c36d9f1d.js
@@ -0,0 +1,1 @@
+import{bg as t,bh as n,bi as o}from"./index-23254bfb.js";function a(e){return typeof e=="string"?new t([document.querySelectorAll(e)],[document.documentElement]):new t([o(e)],n)}export{a as s};
