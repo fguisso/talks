69550b6c85c08fcaffe8893561ccd0d922295be3
fguisso <fguisso@gmail.com>
2024-10-26 17:23:59 +0000

    Deploying to gh-pages from @ fguisso/talks@a71a2aeb2d5a2af6ccc76d2e6918e7936841996f 🚀

---
+++ threat-modeling/assets/slidev/quote-BAO8gYAx.js
@@ -0,0 +1,1 @@
+import{o,b as s,e as t,f as a}from"../modules/vue-CPJF8izW.js";import{u as r}from"./context-Dq3aTO-i.js";const c={class:"slidev-layout quote"},_={class:"my-auto"},i={__name:"quote",setup(l){return r(),(e,n)=>(o(),s("div",c,[t("div",_,[a(e.$slots,"default")])]))}};export{i as _};
