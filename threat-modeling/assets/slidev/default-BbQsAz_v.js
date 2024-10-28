f85ca4ee40fad45f6cb585cc7b48a71696a0b210
fguisso <fguisso@gmail.com>
2024-10-28 01:44:25 +0000

    Deploying to gh-pages from @ fguisso/talks@2b554aa3e5fc204e33699bcee401f8cbd2582d12 🚀

---
+++ threat-modeling/assets/slidev/default-BbQsAz_v.js
@@ -0,0 +1,1 @@
+import{o,b as t,f as s}from"../modules/vue-Bwz3glzd.js";import{_ as r}from"../index-lyhmwtkA.js";const c={},a={class:"slidev-layout default"};function n(e,l){return o(),t("div",a,[s(e.$slots,"default")])}const f=r(c,[["render",n]]);export{f as I};
