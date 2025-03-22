f8fdc13e97ada8cf5dafaa168a484f05acb48ab4
fguisso <fguisso@gmail.com>
2025-03-22 16:46:17 -0300

    update: fix cover title css

--- slides/threat-modeling/layouts/cover.vue
+++ slides/threat-modeling/layouts/cover.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="slidev-layout intro grid">
     <Logo />
-    <div class="flex flex-col justify-center text-center content-center">
+    <div class="flex flex-col justify-center text-center content-center pt-[8em]">
       <slot></slot>
     </div>
       <FooterLink />
