5d95444a2fc69645f317edee2bb09a54e2d15a42
fguisso <fguisso@gmail.com>
2025-04-29 23:44:38 +0000

    Deploying to gh-pages from @ fguisso/talks@a56b5e5eaee293af12b5e50575f895d2adf47589 🚀

---
+++ homelab-proxmox/assets/slidev/default-BWOEUhu2.js
@@ -0,0 +1,1 @@
+import{f as o,o as t,j as s}from"../modules/vue-Cx8njyW6.js";import{_ as r}from"../index-DkB51yHL.js";const c={},a={class:"slidev-layout default"};function n(e,l){return t(),o("div",a,[s(e.$slots,"default")])}const f=r(c,[["render",n]]);export{f as I};
